a3bb94630d7b6493219643e0ca1f9e7b69cf3204
Thomas Fiorilla <engineertf@gmail.com>
2020-10-09 23:12:02 -0400

    Updated memgrind

--- memgrind.c
+++ memgrind.c
@@ -4,87 +4,121 @@
 int main(int argc, char * argv[]){
 	char * testPointer;
 	int i = 0;
+	int j = 0;
 	
-	printf("A: Malloc and free 1 byte 100 times.\n");
-	for(i=0; i < 100; i++){
-		testPointer = malloc(1);
-		free(testPointer);
-	}
-	
-	char * testB[50];
-	printf("B: Malloc 1 byte 50 times.\n");
-	for(i=0; i < 50; i++){
-		testB[i] = (char*)malloc(sizeof(char));
-	}
-	printf("B: Free 1 byte 50 times.\n");
-	for(i=0; i < 50; i++){
-		free(testB[i]);
-		printf("Test B Freed: %d\n", i);
-	}
-	/*
-	printf("C: Randomly malloc or free 50 times each.\n");
-	for(i=0; i < 100; i++){
+	for(j = 0; j < 50; j++){
+		//Test A: malloc 1 and immediately free one byte 120 times
+		printf("A: Malloc and free 1 byte 120 times.\n");
+		for(i=0; i < 120; i++){
+			testPointer = malloc(1);
+			free(testPointer);
+		}
 		
-	}
-	
-	printf("D: Randomly malloc a random size or free 50 times each.\n");
-	for(i=0; i < 100; i++){
 		
-	}
-	*/
-	printf("\nE: Mallocing and freeing into increasingly smaller spaces.\n");
-	
-	testB[0] = (char*)malloc(sizeof(char)*4089);
-	free(testB[0]);
-	
-	testB[0] = (char*)malloc(sizeof(char)*2042);
-	testB[1] = (char*)malloc(sizeof(char)*2041);
-	free(testB[0]);
-	
-	testB[0] = (char*)malloc(sizeof(char)*1018);
-	testB[2] = (char*)malloc(sizeof(char)*1018);
-	free(testB[0]);
-	
-	testB[0] = (char*)malloc(sizeof(char)*506);
-	testB[3] = (char*)malloc(sizeof(char)*506);
-	free(testB[0]);
-	
-	testB[0] = (char*)malloc(sizeof(char)*250);
-	testB[4] = (char*)malloc(sizeof(char)*250);
-	free(testB[0]);
-	
-	testB[0] = (char*)malloc(sizeof(char)*122);
-	testB[5] = (char*)malloc(sizeof(char)*122);
-	free(testB[0]);
-	
-	testB[0] = (char*)malloc(sizeof(char)*58);
-	testB[6] = (char*)malloc(sizeof(char)*58);
-	free(testB[0]);
-	
-	testB[0] = (char*)malloc(sizeof(char)*26);
-	testB[7] = (char*)malloc(sizeof(char)*26);
-	free(testB[0]);
-	
-	testB[0] = (char*)malloc(sizeof(char)*10);
-	testB[8] = (char*)malloc(sizeof(char)*10);
-	free(testB[0]);
-	
-	testB[0] = (char*)malloc(sizeof(char)*2);
-	testB[9] = (char*)malloc(sizeof(char)*2);
-	
-	for(i = 0; i < 10; i++){
-		free(testB[i]);
-		printf("Test B Freed: %d\n", i);
-	}
-	
-	testB[0] = (char*)malloc(sizeof(char)*4089);
-	free(testB[0]);
-	
-	/*
-	printf("F: .\n");
-	for(int i=0; i < 100; i++){
 		
+		//Test B: malloc 1 byte 120 times, then free all of the mallocs
+		printf("B: Malloc 1 byte 120 times.\n");
+		
+		char * testB[120];
+		for(i=0; i < 120; i++){
+			testB[i] = (char*)malloc(sizeof(char));
+		}
+		printf("B: Free 1 byte 120 times.\n");
+		for(i=0; i < 120; i++){
+			free(testB[i]);
+			printf("Test B Freed: %d\n", i);
+		}
+		
+		
+		
+		//Test C: randomly malloc or free 1 byte until 120 mallocs called and they have all been freed
+		printf("C: Randomly malloc or free 120 times each.\n");
+		
+		int mallocCount = 0;
+		int mallocLeft = 0;
+		int freeCount = 0;
+		while(mallocCount < 120){
+			coin = rand();
+			
+			if(coin%2 == 0){
+				mallocCount++;
+				mallocLeft++;
+				
+				testB[mallocCount] = malloc(sizeof(char));
+				
+			} else if(mallocLeft > 0){
+				free(testB[freeCount]
+				
+				mallocLeft--;
+				freeCount++;
+				
+			}
+		}
+		while(freeCount < 120){
+			free(testB[freeCount]
+			
+		}
+		
+		printf("D: Randomly malloc a random size or free 120 times each.\n");
+		for(i=0; i < 100; i++){
+			
+		}
+		
+		
+		
+		//Custom test D: 
+		
+		
+		
+		//Custom test E: calling malloc on a space and splitting one half into further halves each time, then freeing every malloc call before calling malloc for entire size of usable memory
+		printf("\nE: Mallocing and freeing into increasingly smaller spaces.\n");
+		
+		testB[0] = (char*)malloc(sizeof(char)*4089);
+		free(testB[0]);
+		
+		testB[0] = (char*)malloc(sizeof(char)*2042);
+		testB[1] = (char*)malloc(sizeof(char)*2041);
+		free(testB[0]);
+		
+		testB[0] = (char*)malloc(sizeof(char)*1018);
+		testB[2] = (char*)malloc(sizeof(char)*1018);
+		free(testB[0]);
+		
+		testB[0] = (char*)malloc(sizeof(char)*506);
+		testB[3] = (char*)malloc(sizeof(char)*506);
+		free(testB[0]);
+		
+		testB[0] = (char*)malloc(sizeof(char)*250);
+		testB[4] = (char*)malloc(sizeof(char)*250);
+		free(testB[0]);
+		
+		testB[0] = (char*)malloc(sizeof(char)*122);
+		testB[5] = (char*)malloc(sizeof(char)*122);
+		free(testB[0]);
+		
+		testB[0] = (char*)malloc(sizeof(char)*58);
+		testB[6] = (char*)malloc(sizeof(char)*58);
+		free(testB[0]);
+		
+		testB[0] = (char*)malloc(sizeof(char)*26);
+		testB[7] = (char*)malloc(sizeof(char)*26);
+		free(testB[0]);
+		
+		testB[0] = (char*)malloc(sizeof(char)*10);
+		testB[8] = (char*)malloc(sizeof(char)*10);
+		free(testB[0]);
+		
+		testB[0] = (char*)malloc(sizeof(char)*2);
+		testB[9] = (char*)malloc(sizeof(char)*2);
+		
+		for(i = 0; i < 10; i++){
+			free(testB[i]);
+			printf("Test B Freed: %d\n", i);
+		}
+		
+		testB[0] = (char*)malloc(sizeof(char)*4089);
+		free(testB[0]);
 	}
-	*/
+	
 	return 0;
 }
--- mymalloc.c
+++ mymalloc.c
@@ -3,17 +3,23 @@
 
 
 
+static void * allocateMem(int x){
+	
+	
+	return NULL;
+}
+
 
 //THIS IS THE CODE FOR MYMALLOC **************************************************************************************************************************************************************
 void * mymalloc(int x){
 	int location = 0;
 	int location2 = 0;
 	int sizeofMagic = sizeof(*(int*)(blockSim));
-	int sizeofMeta = sizeof(*(short*)(blockSim));
+	int sizeofData = sizeof(*(short*)(blockSim));
 	
 	//If user inputs a number that cannot possibly fit in the space, or is less than 1
     //It is considered too large if it is larger than the size of all the memory, minus the size of the initial magic block, minus the size of the metadata
-	if(x > sizeof(blockSim) - sizeofMagic - sizeofMeta - 1 || x < 1){
+	if(x > sizeof(blockSim) - sizeofMagic - sizeofData - 1 || x < 1){
 		printf("That is larger than the possible space or smaller than 1. Returning with no value.\n");
 		return NULL;
 		
@@ -26,7 +32,7 @@ void * mymalloc(int x){
 		
 		//if the user asks for enough size for magic # and one single metadata block, make the block and don't create metadata after
 		//OR user asks for enough size for magic # and one single metadata block with the size of one metadata block or less after, make the block and don't create metadata after
-		if(x <= sizeof(blockSim) - sizeofMagic - sizeofMeta - 1 && x >= sizeof(blockSim) - sizeofMagic - (2*sizeofMeta) - 2){
+		if(x <= sizeof(blockSim) - sizeofMagic - sizeofData - 1 && x >= sizeof(blockSim) - sizeofMagic - (2*sizeofData) - 2){
 			//printf("	User is asking for exactly enough space to only have one metadata, or not have any reasonable metadata.\n");
 			
 			*(int*)(blockSim) = magic;
@@ -42,7 +48,7 @@ void * mymalloc(int x){
 			
 			*(short*)(blockSim+location) = x;
 			//printf("	Size of user data = %u\n", *(short*)(blockSim+location));
-			location += sizeofMeta;												//casts the user size as a short to the two bytes after the used byte and increments the location
+			location += sizeofData;												//casts the user size as a short to the two bytes after the used byte and increments the location
 			
 			//return location;
 			return &(blockSim[location]);
@@ -63,7 +69,7 @@ void * mymalloc(int x){
 			
 			*(short*)(blockSim+location) = x;
 			//printf("	Size of user data = %u\n", *(short*)(blockSim+location));
-			location += sizeofMeta;												//casts the user size as a short to the two bytes after the used byte and increments the location
+			location += sizeofData;												//casts the user size as a short to the two bytes after the used byte and increments the location
 			
 			location2 = location;
 			location += x;														//gives the current address to location2, adds size of user data to address
@@ -75,7 +81,7 @@ void * mymalloc(int x){
 			
 			//printf("\n	Current location (start of second size-block): %d\n", location);
 			
-			*(short*)(blockSim+location) = sizeof(blockSim) - sizeofMagic - 2 - 2*(sizeofMeta) - x;
+			*(short*)(blockSim+location) = sizeof(blockSim) - sizeofMagic - 2 - 2*(sizeofData) - x;
 			//printf("	Size of remaining data: = %u\n", *(short*)(blockSim + location));
 			
 			//return location2;
@@ -99,10 +105,10 @@ void * mymalloc(int x){
 				location += 1;
 				//printf("	Block is already in use. Size of in-use block: %d\n", *(short*)(blockSim + location));
 				
-				location += *(short*)(blockSim + location) + sizeofMeta;					//reads the actual size value of the block, skips past it
+				location += *(short*)(blockSim + location) + sizeofData;					//reads the actual size value of the block, skips past it
 				
 				//if the location goes out of bounds
-				if(location >= sizeof(blockSim) - 1 - sizeofMeta){
+				if(location >= sizeof(blockSim) - 1 - sizeofData){
 					printf("\n	You have no space left!\n");
 				return NULL;
 				}
@@ -116,18 +122,18 @@ void * mymalloc(int x){
 				
 				//if the first space is the same size or just barely larger than what user asked for (cannot have reasonable metadata afterwards)
 				//it will assign the whole space and return the address to the user, then it won't make a metadata block after
-				if(x <= *(short*)(blockSim+(location+1)) && x + sizeofMeta + 1 >= *(short*)(blockSim+(location+1))){
+				if(x <= *(short*)(blockSim+(location+1)) && x + sizeofData + 1 >= *(short*)(blockSim+(location+1))){
 					//printf("\n  First open space does not have enough space for reasonable metadata after or is the exact size.\n");
 					
 					blockSim[location] = 1;
-					location += (sizeofMeta + 1);
+					location += (sizeofData + 1);
 					
 					//return location;
 					return &(blockSim[location]);
 				}
 				
 				//if the user requests space and the first block has more than enough; gives user space and splits the remaining space into metadata
-				else if(*(short*)(blockSim+(location+1)) > (x + 1 + sizeofMeta)){
+				else if(*(short*)(blockSim+(location+1)) > (x + 1 + sizeofData)){
 					//printf("	Block has enough space and will need to be split.\n");
 				    
 					int size = *(short*)(blockSim+(location+1));								//keeps size of the current block as-is
@@ -136,14 +142,14 @@ void * mymalloc(int x){
 					location += 1;																//sets in-use byte to 1 and iterates
 					
 					*(short*)(blockSim+location) = x;
-					location2 = location + sizeofMeta;											//sets the size block to the size of requested data, sets location2 to address to return to user
+					location2 = location + sizeofData;											//sets the size block to the size of requested data, sets location2 to address to return to user
 					
-					location += x + sizeofMeta;
+					location += x + sizeofData;
 					
 					blockSim[location] = 0;
 					location += 1;																//sets in-use byte of the immediate next metadata block to 0
 					
-					*(short*)(blockSim+location) = size - x - sizeofMeta - 1;
+					*(short*)(blockSim+location) = size - x - sizeofData - 1;
 					
 					//return location2;
 					return &(blockSim[location2]);
@@ -151,10 +157,10 @@ void * mymalloc(int x){
 			}
 			
 			//if the user requests space, the location is free, but does NOT have the available space, it will iterate the location
-			else if(x > *(short*)(blockSim+(location+1) + 1 + sizeofMeta)){
+			else if(x > *(short*)(blockSim+(location+1) + 1 + sizeofData)){
 				//printf("\n	Free location, but not enough space.\n");
 				
-				location += *(short*)(blockSim + (location+1)) + 1 + sizeofMeta;					//reads the actual size value of the block, skips past it
+				location += *(short*)(blockSim + (location+1)) + 1 + sizeofData;					//reads the actual size value of the block, skips past it
 				
 			   	if(location >= 4096){
 					printf("\n	You have no space left!\n\n");
@@ -185,19 +191,19 @@ void myfree(void * x){
 	//free function goes here
 	int location = 0;
 	int sizeofMagic = sizeof(*(int*)(blockSim));
-	int sizeofMeta = sizeof(*(short*)(blockSim));
+	int sizeofData = sizeof(*(short*)(blockSim));
 	
 	//printf("Running free. Address of the value: %d\n", x);
 	
 	//check to see if the address is before or after the limits
-	if(x < &(blockSim[location + sizeofMagic + 1 + sizeofMeta]) || x >= &(blockSim[sizeof(blockSim)-1-sizeofMeta])){
+	if(x < &(blockSim[location + sizeofMagic + 1 + sizeofData]) || x >= &(blockSim[sizeof(blockSim)-1-sizeofData])){
 		printf("	Trying to free memory not within the bounds of memory. Returning.\n");
 		
 		return;
 	}
 	
 	//iterate down the array and check if the address given is the start of a block
-	else if(x >= &(blockSim[0 + sizeofMagic + 1 + sizeofMeta]) || x < &(blockSim[sizeof(blockSim)-1-sizeofMeta])){
+	else if(x >= &(blockSim[0 + sizeofMagic + 1 + sizeofData]) || x < &(blockSim[sizeof(blockSim)-1-sizeofData])){
 		int flag = 0;
 		
 		location += sizeofMagic;		//skips over the size of the magic number
@@ -206,30 +212,30 @@ void myfree(void * x){
 		//create loop for iterating down the array
 		while(flag == 0){
 			//if the user tries to free a location that is not in use, returns
-			if(blockSim[location] == 0 && x == &(blockSim[location + 1 + sizeofMeta])){
+			if(blockSim[location] == 0 && x == &(blockSim[location + 1 + sizeofData])){
 				printf("	Memory address given is not in use! Returning.\n");
 				
 				return;
 			}
 			
 			//if the location's address is lower than the user-given address, iterate to the next location and save the previous location
-			else if(x > &(blockSim[location + 1 + sizeofMeta])){
+			else if(x > &(blockSim[location + 1 + sizeofData])){
 				//printf("	Iterating the array.\n");
 				
 				prevLoc = location;
 				
 				location += 1;
-				location += *(short*)(blockSim + location) + sizeofMeta;
+				location += *(short*)(blockSim + location) + sizeofData;
 				
 				//if the location goes out of bounds
-				if(location >= sizeof(blockSim) - 1 - sizeofMeta){
+				if(location >= sizeof(blockSim) - 1 - sizeofData){
 					printf("\n	!\n");
 					return;
 				}
 			}
 			
 			//if the location's address is in-use and exactly what the user asked for, frees and starts looking through for adjacent free blocks
-			else if(blockSim[location] == 1 && x == &(blockSim[location + 1 + sizeofMeta])){
+			else if(blockSim[location] == 1 && x == &(blockSim[location + 1 + sizeofData])){
 				//printf("	Address confirmed. Freeing the block and checking for adjacent free blocks to merge with.\n");
 				
 				blockSim[location] = 0;
@@ -240,7 +246,7 @@ void myfree(void * x){
 					
 					prevLoc += 1;
 					
-					*(short*)(blockSim+prevLoc) += *(short*)(blockSim+(location+1)) + sizeofMeta + 1;
+					*(short*)(blockSim+prevLoc) += *(short*)(blockSim+(location+1)) + sizeofData + 1;
 					
 					prevLoc -= 1;
 					location = prevLoc;
@@ -268,11 +274,11 @@ void myfree(void * x){
 				
 				//now advance to the next block and check if it's free
 				location += 1;
-				location += *(short*)(blockSim+location) + sizeofMeta;
+				location += *(short*)(blockSim+location) + sizeofData;
 				
 				//if the next block is not free 
 				//OR is out of bounds
-				if(blockSim[location] == 1 || location >= sizeof(blockSim) - 1 - sizeofMeta){
+				if(blockSim[location] == 1 || location >= sizeof(blockSim) - 1 - sizeofData){
 					//printf("	Next location is not free, block has been merged as much as possible.\n");
 					
 					return;
@@ -284,7 +290,7 @@ void myfree(void * x){
 					
 					prevLoc += 1;
 					
-					*(short*)(blockSim+prevLoc) += *(short*)(blockSim+(location+1)) + sizeofMeta + 1;
+					*(short*)(blockSim+prevLoc) += *(short*)(blockSim+(location+1)) + sizeofData + 1;
 					
 					//printf("	Adjacent blocks have all been combined. Returning.\n");
 					
@@ -295,7 +301,7 @@ void myfree(void * x){
 			}
 			
 			//if the location's address is larger than what the user asked for, user is not pointing to the beginning of a location
-			else if(x < &(blockSim[location + 1 + sizeofMeta])){
+			else if(x < &(blockSim[location + 1 + sizeofData])){
 				printf("	Address given to free is not the start of the block. Returning.\n");
 				
 				return;
